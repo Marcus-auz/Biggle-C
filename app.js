26906e8eb0d6bb13a0a2eba5c62d401b4274ab5f
Auz <oo7marcusaurelius@gmail.com>
2019-12-13 12:05:38 +0530

    cors header added to server

--- app.js
+++ app.js
@@ -4,5 +4,12 @@ const feedRoutes=require('./routes/feed');
 const app=express();
 
 app.use(bodyParser.json()); //url encoded is used when incoming data is from a from req type x-www-form-urlencoded
+app.use((req,res,next)=>{
+    res.setHeader('Access-Control-Allow-Origin','*'); //allowing access to all clients 
+    res.setHeader('Access-Control-Allow-Methods','GET,POST,PUT,PATCH,DELETE');
+    res.setHeader('Access-Control-Allow-Headers','Content-type,Authorization');
+    next();
+});
+
 app.use('/feed',feedRoutes);
 app.listen(5000);
